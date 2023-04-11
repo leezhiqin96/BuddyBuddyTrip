9718149680035870e3f661767b27572cd42a4302
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-04-11 11:28:37 +0800

    Added role column to collaborations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_031520) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_11_032644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_031520) do
     t.bigint "itinerary_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", null: false
     t.index ["itinerary_id"], name: "index_collaborations_on_itinerary_id"
     t.index ["user_id"], name: "index_collaborations_on_user_id"
   end
