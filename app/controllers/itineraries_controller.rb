4d9fe7a35d524cf1b09662b7bf9e663d6821d9c9
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-04-15 22:59:59 +0800

    Added new and create methods in itinerary, with only logged in user can create new itinerary

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -2,8 +2,18 @@ class ItinerariesController < ApplicationController
   skip_before_action :authenticate_user!, only: :new
 
   def new
+    @itinerary = Itinerary.new
+    authorize @itinerary
   end
 
   def create
+    @itinerary = Itinerary.new(itinerary_params)
+    authorize @itinerary
+  end
+
+  private
+
+  def itinerary_params
+    params.require(:itinerary).permit(:name, :start_date, :end_date)
   end
 end
