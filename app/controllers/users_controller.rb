1738e93272bb14ff221ac28c4e8e36761eb37592
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-04-17 18:20:08 +0800

    Added policy for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    authorize @user
   end
 
   def edit
+    @user = User.find(params[:id])
+    authorize @user
   end
 
   def update
+    @user = User.find(params[:id])
+    authorize @user
   end
 end
